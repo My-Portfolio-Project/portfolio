8c8a23f11d98ddf51adaf499446ff01ad5b1cd9d
Ajinormotor <teiteiwisdom@gmail.com>
2025-10-01 06:37:38 +0100

    fix: updated expereince data

--- src/constants/data/expereinceData.jsx
+++ src/constants/data/expereinceData.jsx
@@ -9,9 +9,9 @@ export const experienceData = [
         role: "Fullstack Engineer",
         location:"Nigeria(Remote)",
         date:"June 2025 - Present",
-        pointOne: "- Worked as a fullstack engineer  ",
-        pointTwo: "- Collaborated with design team to convert figma designs(dashboard",
-        pointThree: "-assisted in  API intergation",
+        pointOne: "-Translated Figma designs into fully responsive Next.js interfaces and deployed to production. ",
+        pointTwo: "-Built and secured REST API endpoints with optimized caching and RBAC-based authentication.",
+        pointThree: "-Integrated backend REST APIs into the frontend for seamless data flow and user experience",
   
     },
 
@@ -21,9 +21,9 @@ export const experienceData = [
         role: "Frontend Engineer",
         location:"Nigeria(Remote)",
         date:"Feb 2025 - March 2024",
-        pointOne: "- Worked as a frontend engineer using Reactjs ",
-        pointTwo: "- Collaborated with design team to convert figma designs(dashboard",
-        pointThree: "-assisted in  API intergation",
+        pointOne: "-Converted complex Figma designs into functional, pixel-perfect web pages, achieving 100% responsiveness for web and mobile. ",
+        pointTwo: "-Worked with backend developers and UI/UX designers, expediting feature delivery through efficient workflow coordination. ",
+        pointThree: "-Contributed in integrating RESTful APIs, optimizing data fetching and state management, reducing lags in fetching the API. ",
   
     },
 
@@ -32,10 +32,10 @@ export const experienceData = [
         role: "Frontend Engineer",
         location:"Nigeria(Remote)",
         date:"June 2024 - Present",
-        pointOne: "- Worked as a frontend engineer using Nextjs",
-        pointTwo: "- API integration",
-        pointThree: "- Collaborated with design team to convert figma designs",
-        pointFour: "- Converted dashboard designs",
+        pointOne: "-Developed and optimized user interfaces for telecom, payment, and card products using React.js, improving system efficiency and user experience, and cross-device compatibility.",
+        pointTwo: "-Implemented performance optimizations, reducing page load time by 50% through code-splitting, lazy loading, and efficient state management",
+        pointThree: "-Collaborated with UI/UX designers and backend developers, accelerating feature development and deployment speed by 30%.",
+        pointFour: "",
     },
 
     {    id: '4',
@@ -43,10 +43,10 @@ export const experienceData = [
         role: "Frontend Engineer",
         location:"Nigeria(Remote)",
         date:"June 2024 - September 2024",
-        pointOne: "- Worked as a frontend engineer using Reactjs ",
-        pointTwo: "- Collaborated with design team to convert figma designs",
-        pointThree: "- API intergation",
-        pointFour: "- Converted dashboard designs and api integration",
+        pointOne: "-Developed a dynamic and responsive user interface for an eCommerce platform, improving user engagement by 35% through enhanced navigation and UI/UX consistency. ",
+        pointTwo: "-Converted 3 Figma dashboard designs into web pages using React.js, achieving 100% responsiveness across mobile, tablet, and desktop.",
+        pointThree: "-Integrated RESTful APIs, optimizing authentication and dashboard connectivity, which reduced login time by 40% and improved data synchronization",
+        pointFour: "",
     },
 
 ]
